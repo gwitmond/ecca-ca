83e9f9bff3f332ab5fcd21021572c2c402e0ed03
Guido Witmond <guido@witmond.nl>
2013-07-10 12:56:09 +0200

    Certificate now bear the full name: <nickname> @@ <namespace> This is to make them fully worldwide unique.

--- main.go
+++ main.go
@@ -21,12 +21,12 @@ import (
 	"encoding/pem"
 	"io/ioutil"
 	"os"
-	//"html/template"
-	//"database/sql"
 	"time"
 	"github.com/gwenn/gosqlite"
 	"strconv"
 	"flag"
+	"strings"
+	"regexp"
 )
 
 // The things to set before running.
@@ -34,8 +34,9 @@ var configDir = flag.String("config", "example-dir", "Directory where the certif
 var fpcaName = flag.String("fcpa", "exampleFPCA", "Prefix of the FPCA certificate and key file names.")
 var hostname = flag.String("hostname", "register-application.example.nl", "Hostname of the FPCA-registration site. Prefix of the ssl server certificate and key file names.")
 var bindaddress = flag.String("bind", "[::]:443", "Address and port number where to bind the listening socket.") 
+var namespace = flag.String("namespace", "", "Name space that we are signing. I.E. <cn>@@example.com. Specifiy the part after the @@.")
 
-// The certificate and key to sign client certificates with
+// The certificate and key to sign client certificates with. 
 var caCert *Certificate
 var caKey *rsa.PrivateKey
 
@@ -43,6 +44,14 @@ var caKey *rsa.PrivateKey
 func main() {
 	flag.Parse()
 	var err error
+	
+	log.Printf("Parsed parameters: namespace is: %#v\n", *namespace)
+	// validate NameSpace.
+	if *namespace == "" {
+		panic(errors.New("No namespace specified. We need one, otherwise we can't sign certificates!"))
+	}
+	// TODO: validate namespace with FPCA root certificate to prevent mistakes. Panic if wrong.
+
 	// Set the certificate and key to sign client certificates with (and load now to check existence and validity)
 	caCert, err = loadCaCert(*configDir + "/" + *fpcaName +".cert.pem")
 	check(err)
@@ -67,15 +76,36 @@ func main() {
 	check(server6.ListenAndServeTLS(ssl_certificate, ssl_cert_key))
 }
 
+// match  just plain <cn> no @ allowed.
+//var cnRE = regexp.MustCompile(`^([^@]+)(@@([a-zA-Z0-9._]+))?$`)
+//var cnRE = regexp.MustCompile(`^([^@]*)$`)
+//	match := cnRE.FindStringSubmatch(cnvalue)
+//	fmt.Printf("match is %#v\n", match)
+	//username := match[1]
 
 func registerPubkey(w http.ResponseWriter, req *http.Request) {
-	cn     := req.FormValue("cn")
+	cn  := req.FormValue("cn")
+	fmt.Printf("cn is %s\n", cn)
+
 	if cn == "" {
 		http.Error(w, "There is no username! Please specify one.", 400)
 		return
 	}
 
-	// Validate uniqueness of the username (Important requirement for Ecca)
+	if  strings.Contains(cn, "@") {
+		http.Error(w, "We could not recognise your name: it should be without @-sign.", 400)
+		return
+	}
+
+	// TODO: sanitise input, ie, strip leading and trailing white space, collapse all embedded whitespace into a single one.
+	// Perhaps blacklist certain 'sensitive' names such as root/administrator...
+	// Names must be unique.
+	// They must alse be recognisably different for humans.  <cn> == < cn> == <cn    >
+	
+	// This will be the full identity for the user. This is what we sign.
+	cn := fmt.Sprintf("%s@@%s", cn, *namespace)
+	
+	// Validate uniqueness of the CN (Important requirement for Ecca)
 	// BUG(gw): There is a slight race condition between the validation and generation ...
 	nicks := getClients(cn)
 	if len(nicks) > 0 {
@@ -83,7 +113,7 @@ func registerPubkey(w http.ResponseWriter, req *http.Request) {
 		return
 	} 
 
-	// validate the pubkey
+	// Validate that we have a pubkey
 	pubkey := req.FormValue("pubkey")
 	if pubkey == "" {
 		http.Error(w, "There is no Public Key! Please specify one.", 400)
@@ -99,14 +129,14 @@ func registerPubkey(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	// Validation succeeded, sign, store and return certificate
+	// Validation succeeded, sign, store certificate in db and hand it to the requestor
 	cert , err := signCert(cn, publicKey) // sign
 	check(err)
 	writeClient(client{CN: cn, certPEM: cert}) // store
 	
 	w.Header().Set("Content-Type", "text/plain") // return
 	w.Header().Set("Content-Length", strconv.Itoa(len(cert)))
-	w.WriteHeader(201) // set response code
+	w.WriteHeader(201) // set response code to 'Created'
 	w.Write(cert)
 	return
 }
@@ -129,7 +159,7 @@ func signCert(cn string, publicKey interface{}) ([]byte, error) {
                 NotAfter:       time.Now().AddDate(10, 0, 0).UTC(),
 		IsCA:           false,
 		KeyUsage:       KeyUsageDigitalSignature + KeyUsageContentCommitment + KeyUsageDataEncipherment + KeyUsageKeyAgreement,
-		ExtKeyUsage:    []ExtKeyUsage{ExtKeyUsageClientAuth},
+		ExtKeyUsage:    []ExtKeyUsage{ExtKeyUsageClientAuth ExtKeyUsageEmailProtection },
         }
 
 	derBytes, err := CreateClientCertificate(rand.Reader, &template, caCert, publicKey, caKey)
@@ -236,21 +266,6 @@ func loadKey(filename string) (*rsa.PrivateKey, error) {
         block, _ := pem.Decode(der)
         return ParsePKCS1PrivateKey(block.Bytes)
 }
-
-// read certificate file or panic
-// func readCert(certFile string) (*x509.CertPool) {
-// 	pool := x509.NewCertPool()
-//
-// 	certPEMBlock, err := ioutil.ReadFile(certFile)
-// 	if err != nil {
-// 		panic("Cannot read certificate file " + certFile)
-// 	}
-// 	ok := pool.AppendCertsFromPEM(certPEMBlock)
-// 	if !ok  {
-// 		panic("Cannot parse certificate file " + certFile)
-// 	}
-// 	return pool
-// }
 
 var (
         maxInt64 int64 = 0x7FFFFFFFFFFFFFFF
